d61c1c53bda76e4caab582339097c5619da7a1ae
Wang huahua <251154885@qq.com>
2020-05-22 22:49:01 +0800

    spu修改界面动态显示

--- src/views/product/components/SpuForm.vue
+++ src/views/product/components/SpuForm.vue
@@ -6,20 +6,31 @@
     v-show="visible"
   >
     <el-form-item label="SPU名称">
-      <el-input placeholder="SPU名称"></el-input>
+      <el-input placeholder="SPU名称" v-model="form.spuInfo.spuName"></el-input>
     </el-form-item>
     <el-form-item label="品牌">
-      <el-select placeholder="请选择品牌" value="">
-        <el-option label="A" value="1"></el-option>
-        <el-option label="B" value="2"></el-option>
+      <el-select placeholder="请选择品牌" v-model="form.trademarkId">
+        <el-option
+          v-for="t in form.trademarkList"
+          :key="t.id"
+          :label="t.tmName"
+          :value="t.id"
+        ></el-option>
       </el-select>
     </el-form-item>
 
     <el-form-item label="SPU描述">
-      <el-input type="textarea" placeholder="SPU描述" rows="4"></el-input>
+      <el-input
+        type="textarea"
+        placeholder="SPU描述"
+        rows="4"
+        v-model="form.spuInfo.description"
+      ></el-input>
     </el-form-item>
+
     <el-form-item label="SPU图片">
       <el-upload
+        :file-list="form.spuUpImageList"
         action="https://jsonplaceholder.typicode.com/posts/"
         list-type="picture-card"
         :on-preview="handlePictureCardPreview"
@@ -33,23 +44,68 @@
     </el-form-item>
 
     <el-form-item label="销售属性">
-      <el-select placeholder="请选择" value="">
-        <el-option label="A" value="1"></el-option>
-        <el-option label="B" value="2"></el-option>
+      <el-select placeholder="请选择品牌" v-model="form.saleId">
+        <el-option
+          v-for="s in form.saleAttrList"
+          :key="s.id"
+          :label="s.name"
+          :value="s.id"
+        ></el-option>
       </el-select>
       <el-button type="primary" icon="el-icon-plus">添加销售属性</el-button>
 
-      <el-table style="margin-top: 20px" border>
+      <el-table
+        style="margin-top: 20px"
+        border
+        :data="form.spuInfo.spuSaleAttrList"
+      >
         <!-- 序号列 -->
         <el-table-column label="序号" type="index" width="80" align="center">
         </el-table-column>
-        <el-table-column label="属性名"> </el-table-column>
-        <el-table-column label="属性值名称"> </el-table-column>
+        <el-table-column label="属性名" prop="saleAttrName"> </el-table-column>
+
+        <el-table-column label="属性值名称">
+          <template slot-scope="{ row, $index }">
+            <el-tag
+              :key="sAttr.id"
+              v-for="sAttr in row.spuSaleAttrValueList"
+              closable
+              :disable-transitions="false"
+              @close="handleClose(sAttr)"
+            >
+              {{ sAttr.saleAttrValueName }}
+            </el-tag>
+            <el-input
+              class="input-new-tag"
+              v-if="inputVisible"
+              v-model="inputValue"
+              ref="saveTagInput"
+              size="small"
+              @keyup.enter.native="handleInputConfirm"
+              @blur="handleInputConfirm"
+            >
+            </el-input>
+            <el-button
+              v-else
+              class="button-new-tag"
+              size="small"
+              @click="showInput"
+              >+ 添加</el-button
+            >
+          </template>
+        </el-table-column>
+
         <el-table-column label="操作">
           <template slot-scope="{ row, $index }">
-            <el-button type="danger" icon="el-icon-delete" size="mini"
-              >删除</el-button
-            >
+            <el-popconfirm :title="`确定删除属性 吗?`">
+              <hint-button
+                type="danger"
+                title="删除"
+                icon="el-icon-delete"
+                size="mini"
+                slot="reference"
+              ></hint-button>
+            </el-popconfirm>
           </template>
         </el-table-column>
       </el-table>
@@ -77,16 +133,43 @@ export default {
       dialogVisible: false,
 
       form: {
+        saleId: "",
+        trademarkId: "",
         spuId: "", // 当前要更新的spuInfo的id
         spuInfo: {}, // 当前SpuInfo对象
         spuImageList: [], // Spu的图片列表
+        spuUpImageList: [], // Spu已上传的图片列表
         trademarkList: [], //品牌列表
         saleAttrList: [] //销售属性列表
-      }
+      },
+
+      inputVisible: false,
+      inputValue: ""
     };
   },
 
   methods: {
+    //有关销售属性表格的方法
+    handleClose(tag) {
+      this.dynamicTags.splice(this.dynamicTags.indexOf(tag), 1);
+    },
+
+    showInput() {
+      this.inputVisible = true;
+      this.$nextTick(_ => {
+        this.$refs.saveTagInput.$refs.input.focus();
+      });
+    },
+
+    handleInputConfirm() {
+      let inputValue = this.inputValue;
+      if (inputValue) {
+        this.dynamicTags.push(inputValue);
+      }
+      this.inputVisible = false;
+      this.inputValue = "";
+    },
+
     // 由父组件调用的方法,根据id请求加载相关数据
     initLoadUpdateData(spuId) {
       this.spuId = spuId;
@@ -105,6 +188,12 @@ export default {
     async getSpuImageList() {
       const result = await this.$API.sku.getSpuImageList(this.spuId);
       this.form.spuImageList = result.data;
+      this.form.spuUpImageList = result.data.map(item => {
+        let imgList = {};
+        imgList.name = item.imgName;
+        imgList.url = item.imgUrl;
+        return imgList;
+      });
     },
     //获取所有品牌的列表
     async getTrademarkList() {
@@ -117,6 +206,7 @@ export default {
       this.form.saleAttrList = result.data;
     },
 
+    //上传图片所用回调
     handleRemove(file, fileList) {
       console.log(file, fileList);
     },
@@ -133,4 +223,20 @@ export default {
 };
 </script>
 
-<style lang="less" scoped></style>
+<style lang="less" scoped>
+.el-tag + .el-tag {
+  margin-left: 10px;
+}
+.button-new-tag {
+  margin-left: 10px;
+  height: 32px;
+  line-height: 30px;
+  padding-top: 0;
+  padding-bottom: 0;
+}
+.input-new-tag {
+  width: 90px;
+  margin-left: 10px;
+  vertical-align: bottom;
+}
+</style>
